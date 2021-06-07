478bfbc2d319066b0828b0252c83ef4454cb93cd
Shehan Nandathilaka <shehann@codegen.net>
2021-06-07 23:21:38 +0530

    Updated environment variables

--- next.config.js
+++ next.config.js
@@ -4,20 +4,20 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: "develop",
-        mongodb_password: "test",
-        mongodb_clustername: "meetups",
-        mongodb_database: "products",
+        NEXT_PUBLIC_mongodb_username: "develop",
+        NEXT_PUBLIC_mongodb_password: "test",
+        NEXT_PUBLIC_mongodb_clustername: "meetups",
+        NEXT_PUBLIC_mongodb_database: "products",
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: "develop",
-      mongodb_password: "test",
-      mongodb_clustername: "meetups",
-      mongodb_database: "products",
+      NEXT_PUBLIC_mongodb_username: "develop",
+      NEXT_PUBLIC_mongodb_password: "test",
+      NEXT_PUBLIC_mongodb_clustername: "meetups",
+      NEXT_PUBLIC_mongodb_database: "products",
     },
   };
 };
