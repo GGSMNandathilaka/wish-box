13be595c5a83de60adb161b400e35f934be65a3e
Shehan Nandathilaka <shehann@codegen.net>
2021-06-07 22:22:57 +0530

    Implement PUT api req. to mark product as a wish item

---
+++ pages/api/products.js
@@ -0,0 +1,30 @@
+import { MongoClient, ObjectId } from "mongodb";
+// /api/products
+// PATCH /api/products
+
+async function handler(req, res) {
+  if (req.method === "PUT") {
+    const payload = req.body;
+    console.log(payload);
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.movlm.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
+    const client = await MongoClient.connect(connectionString);
+    const db = client.db();
+
+    const meetupsCollection = db.collection("products");
+    const result = await meetupsCollection.findOneAndUpdate(
+      { _id: ObjectId(payload.id) },
+      { $set: payload.data },
+      { upsert: true }
+    );
+
+    console.log(result);
+
+    client.close();
+
+    res.status(200).json({ message: "Product updated successfully" });
+  }
+}
+
+export default handler;
